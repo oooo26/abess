24d5ff9a120f01389a3cdde1e3ab18cd95a16e2f
oooo26 <1160141009@qq.com>
2021-12-29 20:56:55 +0800

    update abessDAG sacrifice

--- src/AlgorithmDAG.h
+++ src/AlgorithmDAG.h
@@ -64,7 +64,7 @@ class abessDAG : public Algorithm<Eigen::VectorXd, Eigen::VectorXd, double, T4> 
 
     bool primary_model_fit(T4 &x, Eigen::VectorXd &y, Eigen::VectorXd &weights, Eigen::VectorXd &beta, double &coef0,
                            double loss0, Eigen::VectorXi &A, Eigen::VectorXi &g_index, Eigen::VectorXi &g_size) {
-        cout << "  --> primary fit" << endl;
+        cout << "  --> primary fit | A = " << A.transpose() << endl;
         int n = x.rows();
         int p = x.cols();
         // MatrixXd Adj = this->compute_Adj(beta, A, p);
@@ -79,19 +79,15 @@ class abessDAG : public Algorithm<Eigen::VectorXd, Eigen::VectorXd, double, T4> 
         for (int i = 0; i < A.size(); i++) {
             if (col != int(A(i) / p)) {  // new node
                 if (ind > 0) {           // update beta
-                    cout << "    lm for node: " << col << endl;
-                    beta.segment(beta_ind, ind) = lm(X_temp.block(0, 0, n, ind), x.col(col));
+                    beta.segment(beta_ind, ind) = this->lm(X_temp.block(0, 0, n, ind), x.col(col));
                     beta_ind += ind;
                 }
                 col = int(A(i) / p);
-                cout << "    new node: " << col << endl;
                 ind = 0;
             }
             X_temp.col(ind++) = x.col(A(i) % p);
         }
-
-        cout << "    lm for node: " << col << endl;
-        beta.segment(beta_ind, ind) = lm(X_temp.block(0, 0, n, ind), x.col(col));
+        beta.segment(beta_ind, ind) = this->lm(X_temp.block(0, 0, n, ind), x.col(col));
         return true;
     };
 
@@ -110,33 +106,76 @@ class abessDAG : public Algorithm<Eigen::VectorXd, Eigen::VectorXd, double, T4> 
                    Eigen::VectorXi &g_size, int N, Eigen::VectorXi &A_ind, Eigen::VectorXd &bd, Eigen::VectorXi &U,
                    Eigen::VectorXi &U_ind, int num) {
         cout << "  --> sacrifice" << endl;
-        // int n = X.rows();
+        int n = X.rows();
         int p = X.cols();
         MatrixXd Adj = this->compute_Adj(beta_A, A, p);
 
-        // forward
-        MatrixXd E = X - X * Adj;
-        E = E.rowwise() - E.colwise().mean();
-        MatrixXd Omega = (E.colwise().norm()).asDiagonal();
-        MatrixXd inv = (MatrixXd::Identity(p, p) - Adj).inverse();  // TODO(hjh): sparse inverse?
-        MatrixXd temp = Omega - inv.adjoint() * this->Sigma * inv;
+        // backward
         for (int i = 0; i < A.size(); i++) {
             int mi = A(i) % p;
             int mj = int(A(i) / p);
-            bd(A(i)) = abs(temp(mi, mj));
+            VectorXd est_old = X * Adj.col(mj).eval();
+            VectorXd est_new = est_old - beta_A(i) * X.col(mi).eval();
+            VectorXd Xj = X.col(mj);
+            bd(A(i)) = (est_new - Xj).squaredNorm() - (est_old - Xj).squaredNorm();
+            cout<<"    backward: ("<<A(i)<<") = "<<bd(A(i))<<endl;
+            // bd(A(i)) = ((est_new + est_old - 2 * Xj).array() * (est_new - est_old).array()).sum();
         }
 
-        // backward
+        // forward
         for (int i = 0; i < I.size(); i++) {
             int mi = I(i) % p;
             int mj = int(I(i) / p);
-            bd(I(i)) = abs(Adj(mi, mj));
+            if (mi == mj) {
+                bd(I(i)) = -DBL_MAX;
+                continue;
+            }
+            VectorXd est_old = X * Adj.col(mj).eval();
+            VectorXd est_new(p);
+            VectorXd Xj = X.col(mj);
+            MatrixXd X_temp(n, p);
+            int ind = 1;
+            X_temp.col(0) = X.col(mi);
+            for (int k = 0; k < p; k++) {
+                if (Adj(k, mj) != 0) {
+                    X_temp.col(ind++) = X.col(k);
+                }
+            }
+            VectorXd beta_temp = this->lm(X_temp.block(0, 0, n, ind), X.col(mj));
+            ind = 1;
+            est_new(mi) = beta_temp(0);
+            for (int k = 0; k < p; k++) {
+                if (Adj(k, mj) != 0) {
+                    est_new(k) = beta_temp(ind++);
+                }
+            }
+            bd(I(i)) = (est_new - Xj).squaredNorm() - (est_old - Xj).squaredNorm();
+            // bd(I(i)) = ((est_new + est_old - 2 * Xj).array() * (est_new - est_old).array()).sum();
         }
 
-        // diag
-        for (int i = 0; i < p; i++) {
-            bd(i * p + i) = -DBL_MAX;
-        }
+        // // forward
+        // MatrixXd E = X - X * Adj;
+        // E = E.rowwise() - E.colwise().mean();
+        // MatrixXd Omega = (E.colwise().norm()).asDiagonal();
+        // MatrixXd inv = (MatrixXd::Identity(p, p) - Adj).inverse();  // TODO(hjh): sparse inverse?
+        // MatrixXd temp = Omega - inv.adjoint() * this->Sigma * inv;
+        // for (int i = 0; i < A.size(); i++) {
+        //     int mi = A(i) % p;
+        //     int mj = int(A(i) / p);
+        //     bd(A(i)) = abs(temp(mi, mj));
+        // }
+
+        // // backward
+        // for (int i = 0; i < I.size(); i++) {
+        //     int mi = I(i) % p;
+        //     int mj = int(I(i) / p);
+        //     bd(I(i)) = abs(Adj(mi, mj));
+        // }
+
+        // // diag
+        // for (int i = 0; i < p; i++) {
+        //     bd(i * p + i) = -DBL_MAX;
+        // }
     };
 
     MatrixXd compute_Adj(VectorXd &beta_A, VectorXi &A, int p) {
